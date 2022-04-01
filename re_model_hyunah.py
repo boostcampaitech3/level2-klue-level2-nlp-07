67c381cb54a9558fc61eca54b607a4e68f34d986
HyunAh-Kim-Clou <wgrape0@gmail.com>
2022-04-01 06:56:30 +0000

    add re_model_hyunah file

---
+++ re_model_hyunah.py
@@ -0,0 +1,89 @@
+import torch
+import torch.nn as nn
+from transformers import AutoModel, AutoConfig, AutoModelForSequenceClassification, BertForSequenceClassification
+from torch.cuda.amp import autocast
+from torch.utils.data import DataLoader
+from torch.autograd import Variable
+import numpy as np
+
+
+class ReModel(nn.Module):
+    def __init__(self, args, tokenizer):
+        super().__init__()
+        self.args = args
+        
+        MODEL_NAME = args.model
+        self.config =  AutoConfig.from_pretrained(MODEL_NAME)
+        self.num_labels = args.num_labels
+        self.model =  AutoModel.from_pretrained(MODEL_NAME, config=self.config)
+        self.model.resize_token_embeddings(len(tokenizer))
+        
+        hidden_size = self.config.hidden_size
+        batch_size = args.per_device_train_batch_size
+        dropout_prob = self.config.attention_probs_dropout_prob
+        
+        self.loss_fn = nn.CrossEntropyLoss()
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(hidden_size * 4, hidden_size),
+            nn.ReLU(),
+            nn.Dropout(p=dropout_prob),
+            nn.Linear(hidden_size, self.config.num_labels)
+        )
+        
+    
+    @autocast()
+    def forward(
+        self,
+        input_ids=None,
+        attention_mask=None,
+        token_type_ids=None,
+        entity_position_embedding = None,
+        position_ids=None,
+        head_mask=None,
+        inputs_embeds=None,
+        labels=None,
+        output_attentions=None,
+        output_hidden_states=None,
+        return_dict=None,
+    ):
+        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
+
+        outputs = self.model(
+            input_ids,
+            attention_mask=attention_mask,
+            token_type_ids=token_type_ids,
+        )
+
+        pooled_output = outputs[0] # logits
+        print('---- pooled_output shape: ', pooled_output.shape) # 16, 256, 768
+        print('---- entity_position_embedding.shape: ', entity_position_embedding.shape) # 16, 4
+
+        idx = torch.arange(input_ids.size(0)).to(input_ids.device)
+        embs = []
+        for x in entity_position_embedding.T:
+            embs.append( torch.tensor([pooled_output[idx, int(y)].tolist() for y in x]) )
+        print('---- embs.len: ', len(embs)) # 16, 4
+        
+        h = torch.cat((embs[0], embs[1], embs[2], embs[3]), dim=-1).to(input_ids.device)
+        print('---- h.shape: ', h.shape, h.device) # 16, 4
+        
+        logits = self.classifier(h)
+        print('---- hidden_logits shape: ', logits.shape) # 16, 256, 768
+        
+        if labels is not None:
+            print('---- if labels- logits:', logits.shape, ', labels: ', labels)
+            loss = self.loss_fn(logits.float(), labels)
+        
+        if not return_dict:
+            print('---- if return_dict- logits:', logits.shape, ', labels: ', labels)
+            output = (logits,) + outputs[2:]
+            loss = self.loss_fn(logits.float(), labels)
+            return ((loss,) + output) if loss is not None else output
+
+        return SequenceClassifierOutput(
+            loss=loss,
+            logits=logits,
+            hidden_states=outputs.hidden_states,
+            attentions=outputs.attentions,
+        )
